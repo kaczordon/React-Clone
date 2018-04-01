602900dc637b2557779efa01a268697183a1d16b
kaczordon <jakub.kawalec@yahoo.com>
2018-03-31 17:40:00 -0700

    render method

---
+++ render.js
@@ -0,0 +1,21 @@
+function render(element, parentDom) {
+    const { type, props } = element;
+    const dom = document.createElement(type);
+    
+    const isListener = name => name.startsWith("on");
+    Object.keys(props).filter(isListener).forEach(name => {
+        const eventType = name.toLowerCase().substr(2);
+        dom.addEventListener(eventType, props[name])
+    })
+
+    const isAtrribute = name => !isListener(name) && name != "children";
+    Object.keys(name).filter(isAtrribute).forEach(name => {
+        dom[name] = props[name];
+    })
+
+
+    const childElements = element.children || [];
+    childElements.forEach(child => render(child, element));
+    
+    parentDom.appendChild(dom);
+}
