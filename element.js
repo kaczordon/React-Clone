7435bf29d0d5b2363e4cf18efda28d9bb3cf8543
kaczordon <jakub.kawalec@yahoo.com>
2018-04-21 17:08:02 -0700

    new render

--- element.js
+++ element.js
@@ -4,7 +4,7 @@ function createElement(type, config, ...args) {
     const props = Object.assign({}, config);
     const hasChildren = args.length > 0;
     const rawChildren = hasChildren ? [].concat(...args) : [];
-    
+
     props.children = rawChildren
         .filter(c => c != null && c!== false)
             .map(c => c instanceof Object ? c : createTextElement(c));
@@ -14,4 +14,4 @@ function createElement(type, config, ...args) {
 
 function createTextElement(value) {
     return createElement(TEXT_ELEMENT, { nodeValue: value });
-  }
+}
--- render.js
+++ render.js
@@ -1,25 +1,66 @@
-function render(element, parentDom) {
+let rootInstance = null;
+
+function render(element, container) {
+	const prevInstance = rootInstance;
+	const nextInstance = reconcile(prevInstance, container, element);
+	rootInstance = nextInstance;
+}
+
+function reconcile(instance, parentDom, element) {
+	if(instance == null) {
+		const newInstance = instantiate(element);
+		parentDom.appendChild(newInstance.dom);
+		return newInstance;
+	} else {
+		const newInstance = instantiate(element);
+		parentDom.replaceChild(newInstance.dom, instance.dom);
+		return newInstance;
+	}
+}
+
+function instantiate(element) {
     const { type, props } = element;
-    
+
     const isTextElement = type === "TEXT ELEMENT";
     const dom = isTextElement
         ? document.createTextNode("")
         : document.createElement(type);
 
-    const isListener = name => name.startsWith("on");
-    Object.keys(props).filter(isListener).forEach(name => {
-        const eventType = name.toLowerCase().substr(2);
-        dom.addEventListener(eventType, props[name])
-    })
+    updateDomProperties(dom, [], props);
 
-    const isAtrribute = name => !isListener(name) && name != "children";
-    Object.keys(name).filter(isAtrribute).forEach(name => {
-        dom[name] = props[name];
-    })
+    const childElements = props.children || [];
+    const childInstances = childElements.map(instantiate);
+    const childDoms = childInstances.map(childInstance => childInstance.dom);
+    childDoms.forEach(childDom => dom.appendChild(childDom));
+
+    const instance = {dom, element, childInstances};
+    return instance;
+}
 
 
-    const childElements = element.children || [];
-    childElements.forEach(child => render(child, element));
-    
-    parentDom.appendChild(dom);
+function updateDomProperties(dom, prevProps, nextProps) {
+  const isEvent = name => name.startsWith("on");
+  const isAttribute = name => !isEvent(name) && name != "children";
+
+  // Remove event listeners
+  Object.keys(prevProps).filter(isEvent).forEach(name => {
+    const eventType = name.toLowerCase().substring(2);
+    dom.removeEventListener(eventType, prevProps[name]);
+  });
+
+  // Remove attributes
+  Object.keys(prevProps).filter(isAttribute).forEach(name => {
+    dom[name] = null;
+  });
+
+  // Set attributes
+  Object.keys(nextProps).filter(isAttribute).forEach(name => {
+    dom[name] = nextProps[name];
+  });
+
+  // Add event listeners
+  Object.keys(nextProps).filter(isEvent).forEach(name => {
+    const eventType = name.toLowerCase().substring(2);
+    dom.addEventListener(eventType, nextProps[name]);
+  });
 }
