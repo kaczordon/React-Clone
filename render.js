b77cd0ecaf59c227b89d99c8c12c8422a4f649f2
kaczordon <jakub.kawalec@ibm.com>
2018-04-21 18:50:04 -0700

    add instances

--- render.js
+++ render.js
@@ -11,11 +11,36 @@ function reconcile(instance, parentDom, element) {
 		const newInstance = instantiate(element);
 		parentDom.appendChild(newInstance.dom);
 		return newInstance;
-	} else {
+	} else if (element == null) {
+    // Remove instance
+    parentDom.removeChild(instance.dom);
+    return null;
+  } else if (instance.element.type === element.type) {
+    // Update instance
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.childInstances = reconcileChildren(instance, element);
+    instance.element = element;
+    return instance;
+} else {
 		const newInstance = instantiate(element);
 		parentDom.replaceChild(newInstance.dom, instance.dom);
 		return newInstance;
 	}
+}
+
+function reconcileChildren(instance, element) {
+  const dom = instance.dom;
+  const childInstances = instance.childInstances;
+  const nextChildElements = element.props.children || [];
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+  for(let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+    const newChildInstance = reconcile(dom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+  return newChildInstances.filter(instance => instance != null);
 }
 
 function instantiate(element) {
